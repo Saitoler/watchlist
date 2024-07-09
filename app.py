77fcf8d0ff20e99250eadac76736787cd5836c1e
saitoler <1402145603@qq.com>
2024-07-09 19:34:25 +0800

    添加 index.html 页面

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 from markupsafe import escape
 
 # 实例化 Flask 类，创建一个 程序 对象 app
 app = Flask(__name__)
 
+name = "Saitoler"
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 
 # 视图函数(view function) -- 请求处理函数
 @app.route('/')
-def hello():  # put application's code here
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
-
+def index():  # put application's code here
+    # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
 
 @app.route('/home')
 def home():  # put application's code here
