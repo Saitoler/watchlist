e524bdf436000a8470b34d1b13fa1d460a2f3ba6
saitoler <1402145603@qq.com>
2024-07-11 20:58:35 +0800

    设置flask 连接 mysql 数据库, 并将虚拟数据写入到 mysql 中

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, url_for, render_template
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
+import click
 
 # 实例化 Flask 类，创建一个 程序 对象 app
 app = Flask(__name__)
+
+# 设置数据库连接地址
+mysql_url = "mysql+pymysql://root:wxc.469713@127.0.0.1:3306/watchlist"
+app.config['SQLALCHEMY_DATABASE_URI'] = mysql_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # 是否追踪数据库修改, 一般不开启，会影响性能
+
+# 初始化 db, 关联 flask 项目
+db = SQLAlchemy(app)
 
 name = "Saitoler"
 movies = [
@@ -18,12 +28,54 @@ movies = [
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
 
+# 创建数据库模型
+class User(db.Model):  # 表名将会是 user (自动生成，小写处理)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+
+# 自定义命令 initdb 用于初始化 db
+@app.cli.command()  # 注册为命令 可以传入 name 参数来自定义命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置命令行选项
+def initdb(drop):
+    if drop:
+        db.drop_all()
+
+    db.create_all()
+    click.echo("已初始化数据库")
+
+
+# 自定义命令用于将虚拟数据添加到数据库里
+@app.cli.command()
+def forge():
+    db.create_all()
+
+    # 将 User 写入 db
+    user = User(name=name)
+    db.session.add(user)
+
+    # 将 movie 写入 db
+    for movie in movies:
+        movie = Movie(title=movie['title'], year=movie['year'])
+        db.session.add(movie)
+
+    # 提交数据
+    db.session.commit()
+    click.echo("虚拟数据已写入数据库中.")
+
 
 # 视图函数(view function) -- 请求处理函数
 @app.route('/')
 def index():  # put application's code here
     # return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
     return render_template('index.html', name=name, movies=movies)
+
 
 @app.route('/home')
 def home():  # put application's code here
